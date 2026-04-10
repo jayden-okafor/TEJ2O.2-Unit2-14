33b550d3c5a602bba55753ab6dce7eaeea02b375
jayden-okafor <jayden.okafor@stu.ocsb.ca>
2026-04-10 12:54:03 -0400

    added comments

--- main.ts
+++ main.ts
@@ -14,24 +14,28 @@ let loopCounterY: number = 0
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
+// when button "A" is pressed
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
     sprite = game.createSprite(0, 0)
     loopCounterX = 0
     loopCounterY = 0
 
+// move the sprite right by 1 each time
     while (loopCounterX < 5) {
         sprite.set(LedSpriteProperty.X, loopCounterX)
         loopCounterX++
         basic.pause(500)
     }
 
+// move the sprite down by 1 each time
     while (loopCounterY < 5) {
         sprite.set(LedSpriteProperty.Y, loopCounterY)
         loopCounterY++
         basic.pause(500)
     }
 
+// move the sprite left by 1 each time
     if (loopCounterX == 5) {
         while (loopCounterX >= 0) {
             sprite.set(LedSpriteProperty.X, loopCounterX)
@@ -40,6 +44,7 @@ input.onButtonPressed(Button.A, function () {
         }
     }
 
+// move the sprite right by 1 each time
     if (loopCounterY == 5) {
         while (loopCounterY >= 0) {
             sprite.set(LedSpriteProperty.Y, loopCounterY)
@@ -48,29 +53,34 @@ input.onButtonPressed(Button.A, function () {
         }
     }
 
+// remove sprite and show happy face
     sprite.delete()
     basic.clearScreen()
     basic.showIcon(IconNames.Happy)
 })
 
+// when the "B" button is pressed
 input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
     sprite = game.createSprite(0, 0)
     loopCounterX = 0
     loopCounterY = 0
 
+// move the sprite down by 1 each time
     while (loopCounterY < 5) {
         sprite.set(LedSpriteProperty.Y, loopCounterY)
         loopCounterY++
         basic.pause(500)
     }
 
+// move the sprite right by 1 each time
     while (loopCounterX < 5) {
         sprite.set(LedSpriteProperty.X, loopCounterX)
         loopCounterX++
         basic.pause(500)
     }
 
+// move the sprite up by 1 each time
     if (loopCounterY == 5) {
         while (loopCounterY >= 0) {
             sprite.set(LedSpriteProperty.Y, loopCounterY)
@@ -79,6 +89,7 @@ input.onButtonPressed(Button.B, function () {
         }
     }
 
+// move the sprite left by 1 each time
     if (loopCounterX == 5) {
         while (loopCounterX >= 0) {
             sprite.set(LedSpriteProperty.X, loopCounterX)
@@ -87,6 +98,7 @@ input.onButtonPressed(Button.B, function () {
         }
     }
 
+// remove sprite and show happy face
     sprite.delete()
     basic.clearScreen()
     basic.showIcon(IconNames.Happy)
