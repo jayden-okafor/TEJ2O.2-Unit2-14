b7b4d9cb869d46e263b87afc62833b419c9eb441
jayden-okafor <jayden.okafor@stu.ocsb.ca>
2026-04-14 00:17:03 +0000

    edited python

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -7,8 +7,6 @@
 from microbit import *
 
 # variables
-sprite_x = 0
-sprite_y = 0
 loopCounterX = 0
 loopCounterY = 0
 
@@ -19,72 +17,76 @@ display.show(Image.HAPPY)
 while True:
     if button_a.was_pressed():
         display.clear()
-        sprite_x = 0
-        sprite_y = 0
         loopCounterX = 0
         loopCounterY = 0
 
         while loopCounterX < 5:
-            sprite_x = loopCounterX
-            display.set_pixel(sprite_x, sprite_y, 9)
+            display.clear()
+            display.set_pixel(loopCounterX, loopCounterY, 9)
             loopCounterX += 1
             sleep(500)
 
+        loopCounterX -= 1  # fix position
+
         while loopCounterY < 5:
-            sprite_y = loopCounterY
-            display.set_pixel(sprite_x, sprite_y, 9)
+            display.clear()
+            display.set_pixel(loopCounterX, loopCounterY, 9)
             loopCounterY += 1
             sleep(500)
 
-        if loopCounterX == 5:
-            while loopCounterX >= 0:
-                sprite_x = loopCounterX
-                display.set_pixel(sprite_x, sprite_y, 9)
-                loopCounterX -= 1
-                sleep(500)
+        loopCounterY -= 1  # fix position
 
-        if loopCounterY == 5:
-            while loopCounterY >= 0:
-                sprite_y = loopCounterY
-                display.set_pixel(sprite_x, sprite_y, 9)
-                loopCounterY -= 1
-                sleep(500)
+        while loopCounterX >= 0:
+            display.clear()
+            display.set_pixel(loopCounterX, loopCounterY, 9)
+            loopCounterX -= 1
+            sleep(500)
+
+        loopCounterX += 1  # fix position
+
+        while loopCounterY >= 0:
+            display.clear()
+            display.set_pixel(loopCounterX, loopCounterY, 9)
+            loopCounterY -= 1
+            sleep(500)
 
         display.clear()
         display.show(Image.HAPPY)
 
     if button_b.was_pressed():
         display.clear()
-        sprite_x = 0
-        sprite_y = 0
         loopCounterX = 0
         loopCounterY = 0
 
         while loopCounterY < 5:
-            sprite_y = loopCounterY
-            display.set_pixel(sprite_x, sprite_y, 9)
+            display.clear()
+            display.set_pixel(loopCounterX, loopCounterY, 9)
             loopCounterY += 1
             sleep(500)
 
+        loopCounterY -= 1
+
         while loopCounterX < 5:
-            sprite_x = loopCounterX
-            display.set_pixel(sprite_x, sprite_y, 9)
+            display.clear()
+            display.set_pixel(loopCounterX, loopCounterY, 9)
             loopCounterX += 1
             sleep(500)
 
-        if loopCounterY == 5:
-            while loopCounterY >= 0:
-                sprite_y = loopCounterY
-                display.set_pixel(sprite_x, sprite_y, 9)
-                loopCounterY -= 1
-                sleep(500)
+        loopCounterX -= 1
 
-        if loopCounterX == 5:
-            while loopCounterX >= 0:
-                sprite_x = loopCounterX
-                display.set_pixel(sprite_x, sprite_y, 9)
-                loopCounterX -= 1
-                sleep(500)
+        while loopCounterY >= 0:
+            display.clear()
+            display.set_pixel(loopCounterX, loopCounterY, 9)
+            loopCounterY -= 1
+            sleep(500)
+
+        loopCounterY += 1
+
+        while loopCounterX >= 0:
+            display.clear()
+            display.set_pixel(loopCounterX, loopCounterY, 9)
+            loopCounterX -= 1
+            sleep(500)
 
         display.clear()
         display.show(Image.HAPPY)
