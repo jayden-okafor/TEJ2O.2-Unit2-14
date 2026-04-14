8b8c79a4c311b10201fc53a128a02eec9932a2e3
jayden-okafor <jayden.okafor@stu.ocsb.ca>
2026-04-14 00:28:53 +0000

    added more comments

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -14,79 +14,85 @@ loopCounterY = 0
 display.clear()
 display.show(Image.HAPPY)
 
+# when button "A" is pressed
 while True:
     if button_a.was_pressed():
         display.clear()
         loopCounterX = 0
         loopCounterY = 0
 
+        # move the sprite right by 1 each time
         while loopCounterX < 5:
             display.clear()
             display.set_pixel(loopCounterX, loopCounterY, 9)
             loopCounterX += 1
             sleep(500)
+        loopCounterX -= 1
 
-        loopCounterX -= 1  # fix position
-
+        # move the sprite down by 1 each time
         while loopCounterY < 5:
             display.clear()
             display.set_pixel(loopCounterX, loopCounterY, 9)
             loopCounterY += 1
             sleep(500)
+        loopCounterY -= 1
 
-        loopCounterY -= 1  # fix position
-
+        # move the sprite left by 1 each time
         while loopCounterX >= 0:
             display.clear()
             display.set_pixel(loopCounterX, loopCounterY, 9)
             loopCounterX -= 1
             sleep(500)
+        loopCounterX += 1
 
-        loopCounterX += 1  # fix position
-
+        # move the sprite right by 1 each time
         while loopCounterY >= 0:
             display.clear()
             display.set_pixel(loopCounterX, loopCounterY, 9)
             loopCounterY -= 1
             sleep(500)
 
+        # remove sprite and show happy face
         display.clear()
         display.show(Image.HAPPY)
 
+    # when the "B" button is pressed
     if button_b.was_pressed():
         display.clear()
         loopCounterX = 0
         loopCounterY = 0
 
+        # move the sprite down by 1 each time
         while loopCounterY < 5:
             display.clear()
             display.set_pixel(loopCounterX, loopCounterY, 9)
             loopCounterY += 1
             sleep(500)
-
         loopCounterY -= 1
 
+        # move the sprite right by 1 each time
         while loopCounterX < 5:
             display.clear()
             display.set_pixel(loopCounterX, loopCounterY, 9)
             loopCounterX += 1
             sleep(500)
-
         loopCounterX -= 1
 
+        # move the sprite up by 1 each time
         while loopCounterY >= 0:
             display.clear()
             display.set_pixel(loopCounterX, loopCounterY, 9)
             loopCounterY -= 1
             sleep(500)
-
         loopCounterY += 1
 
+        # move the sprite left by 1 each time
         while loopCounterX >= 0:
             display.clear()
             display.set_pixel(loopCounterX, loopCounterY, 9)
             loopCounterX -= 1
             sleep(500)
 
+        # remove sprite and show happy face
         display.clear()
         display.show(Image.HAPPY)
